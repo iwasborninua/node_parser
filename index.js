71ad5fb66407cf7c6e08a3f43546de066a05a207
Michael <mihail.macnamara@gmail.com>
2020-09-09 21:06:27 +0300

    09.09,2020

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const needle = require('needle');
 const fs = require('fs');
 
 let from = new Date('2006-02-01');
-let to = new Date('2006-02-02');
+let to = new Date('2006-05-02');
 let url = null;
 
 while (from < to) {
     url = 'https://whoistory.com/' + from.getFullYear() + '/' + ("0" + (from.getMonth() + 1)) + '/' + (from.getDate() < 10 ? "0" + from.getDate(): from.getDate());
-
+    console.log(url);
     needle.get(url, function(err, res){
         if (err) throw err;
-        console.log(res.body);
-        console.log(res.statusCode);
     });
 
     from.setDate(from.getDate() + 1);
